38aff5115b3461fa380d87894ee5ee02fbae0a23
Ray1yn <1175482512@qq.com>
2021-11-10 04:00:36 +0800

    P15

--- src/main/java/com/dbcourse/community/controller/AuthorizeController.java
+++ src/main/java/com/dbcourse/community/controller/AuthorizeController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -22,9 +25,12 @@ public class AuthorizeController {
     @Value("${github.redirect.url}")
     private String redirectUrl;
 
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name ="state")String state){
+                           @RequestParam(name ="state")String state,
+                           HttpServletRequest request
+                           ){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
@@ -35,7 +41,16 @@ public class AuthorizeController {
 
         String accessToken =githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+
+        if(user != null){
+            //登录成功，写cookie 和 session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登陆
+            return "redirect:/";
+        }
+
+
     }
 }
--- src/main/java/com/dbcourse/community/provider/GithubProvider.java
+++ src/main/java/com/dbcourse/community/provider/GithubProvider.java
@@ -24,8 +24,8 @@ public class GithubProvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
            String string =  response.body().string();
-            String token = string.split("&")[0].split("=")[1];
-            return token;
+           String token = string.split("&")[0].split("=")[1];
+           return token;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,6 +44,7 @@ public class GithubProvider {
             String string =  response.body().string();
             GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
             return githubUser;
+
         } catch (IOException e) {
             e.printStackTrace();
         }
